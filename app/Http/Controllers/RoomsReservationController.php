057429a4e45e9e5bff8e1857ea8f56ca885b8e0b
Beemo44 <louis.mornet@orange.fr>
2025-03-28 12:25:18 +0100

    Rooms reservation controller update

--- app/Http/Controllers/RoomsReservationController.php
+++ app/Http/Controllers/RoomsReservationController.php
@@ -13,4 +13,72 @@ class RoomsReservationController extends Controller
             ->where('room_id',$roomId)
             ->first();
     }
+
+    public function getUserReservations($userId)
+    {
+        return RoomsReservation::where('user_id', $userId)->get();
+    }
+
+    private function validate($request){
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'room_id' => 'required|exists:rooms,id',
+            'date' => 'required|date'
+        ]);
+
+        if($validator->fails()){
+            return $validator->errors();
+        }
+
+        return true;
+    }
+
+    public function index()
+    {
+        $reservations = BooksReservation::all();
+        return response()->json($reservations);
+    }
+
+    public function showReservations() {
+        $reservations = $this->getUserReservations(Auth::user()->id);
+        return response()->json($reservations);
+    }
+
+    public function showUserReservations($userId) {
+        $reservations = $this->getUserReservations($userId);
+        return response()->json($reservations);
+    }
+
+    public function store(Request $request)
+    {
+        $validation = $this->validate($request);
+        if($validation !== true) {
+            return response()->json($validation,400);
+        }
+
+        if(RoomsReservation::where('room_id', $request->room_id)
+            ->Where('date')
+            ->exists()) {
+            return response()->json(['error' => 'Room is already reserved'], 400);
+            }
+        
+        $reservation = RoomsReservation::create([
+            'user_id' => Auth::user()->id,
+            'room_id' => $request->room_id,
+            'date' => $request->date,
+        ]);
+
+        return response()->json($reservation, 201);
+    }
+
+    public function destroy($reservationId) {
+        $reservation = RoomsReservation::where('id',$reservationId)->get();
+        if($reservation === null) {
+            return response()->json(['error' => 'Reservation not found'], 404);
+
+        }
+
+        $reservation->delete();
+        return reponse()->json(['message' => 'Reseravation deleted']);
+    }
 }
