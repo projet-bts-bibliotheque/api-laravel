08dca3ad4aaa87a9e80e542a68b0a58be1022981
JustStop__ <contact@juststop.dev>
2025-05-28 14:54:25 +0200

    fix missing imports

--- app/Http/Controllers/RoomsReservationController.php
+++ app/Http/Controllers/RoomsReservationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RoomsReservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RoomsReservationController extends Controller
 {
