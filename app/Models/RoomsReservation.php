c494814b095247ee6469e7d97e01a3cae1e3cc86
Beemo <161473712+Beemo44@users.noreply.github.com>
2025-05-28 09:02:38 +0200

    Update RoomsReservation.php

--- app/Models/RoomsReservation.php
+++ app/Models/RoomsReservation.php
@@ -28,7 +28,7 @@ class RoomsReservation extends Model
 
     public function user()
     {
-        return $this-belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function room()
