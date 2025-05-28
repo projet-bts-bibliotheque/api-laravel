f7c1d1b1256fb2dcba4579c567265eb78ffc7e93
JustStop__ <contact@juststop.dev>
2025-05-28 15:14:59 +0200

    fix missing model import

--- app/Http/Controllers/RoomsReservationController.php
+++ app/Http/Controllers/RoomsReservationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\RoomsReservation;
 use Illuminate\Http\Request;
+use App\Models\RoomsReservation;
+use App\Models\BooksReservation;
 use Illuminate\Support\Facades\Validator;
 
 class RoomsReservationController extends Controller
